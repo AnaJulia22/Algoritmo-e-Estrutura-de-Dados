e457bb9d8074c584ab9b6c74bf4efb5ab3f70498
AnaJulia22 <anajulia11.o.fernandes@gmail.com>
2023-05-12 15:01:01 -0300

    adding the problem context

--- Vetores, filas e pilhas/O Anti-Sistema de Ed. Fisica.py
+++ Vetores, filas e pilhas/O Anti-Sistema de Ed. Fisica.py
@@ -1,3 +1,24 @@
+# Nesse contexto, desenvolva uma função novaLocacao(pilha, codigo) que adiciona, em tempo O(n) o valor inteiro ‘codigo’
+# na sua posição apropriada da pilha ‘pilha’ (vetor), se a pilha estiver ordenada.
+# A função deve retornar um novo vetor com a nova locação adicionada.
+# Mas, atenção, antes disso você deve executar uma função pilhaImaculada(pilha) que checa em tempo O(n) se ela estava
+# na ordem certa, antes de fazer a inserção.
+#
+# Input
+#
+# A entrada do programa é composta por duas linhas.
+# A primeira representa o vetor que se refere à pilha de solicitações, com números inteiros (códigos de locação)
+# separados por vírgula e sem espaço, no formato a,b,c.
+# A segunda linha é o código da nova locação, que é um inteiro simples
+# que deverá ser inserido na lista, se esta estiver ordenada.
+#
+# Output
+#
+# Você deve produzir uma única linha de saída. Caso a pilha tenha sido encontrada ordenada e a
+# nova solicitação tenha sido inserida nela, deve-se imprimir a nova pilha de solicitações,
+# no mesmo formato da entrada (['a','b','c']). Caso a pilha tenha sido encontrada desordenada,
+# imprima a expressão “A pilha está um caos.”.
+
 def novaLocacao(pilha, codigo):
     counter = int()
     pilha_ok = False
